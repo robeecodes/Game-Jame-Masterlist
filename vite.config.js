d5f6f29b2cf6888dc57005f79a2693da0dc08e8d
Robin <97387960+robeecodes@users.noreply.github.com>
2024-11-07 22:50:06 +0000

    fix(vite.config.js): remove false entry-point

--- vite.config.js
+++ vite.config.js
@@ -11,12 +11,6 @@ export default defineConfig({
         },
     build:
         {
-            rollupOptions: {
-                input: {
-                    main: '/index.html',
-                    apseDesigns: '/apse-designs.html',
-                },
-            },
             outDir: '../dist', // Output in the dist/ folder
             emptyOutDir: true, // Empty the folder first
             sourcemap: true // Add sourcemap
